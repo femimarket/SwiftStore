187030a02a1530741180387c4a3f798b915976db
femimarket <BJTDhwzXnYx7TLwQdyhVoArwbQnNAMJj2gq5Zs5hpump@femi.market>
2026-06-21 22:58:36 +0100

    Initial Commit

---
+++ Store/StoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoreApp.swift
+//  Store
+//
+//  Created by u on 21/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct StoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
